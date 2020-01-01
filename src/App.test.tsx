4ee62ddc72544c0069ca053004eee2272bdddd35
Mat Sz <contact@matsz.dev>
2020-01-01 15:52:21 +0100

    added a render test

---
+++ src/App.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import App from './App';
+import createStore from './store';
+
+describe('application', () => {
+    it('renders without crashing', () => {
+        const store = createStore();
+        
+        const div = document.createElement('div');
+        ReactDOM.render(
+            <Provider store={store}>
+                <App />
+            </Provider>, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+});
